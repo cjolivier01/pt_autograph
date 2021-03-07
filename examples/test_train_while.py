ce09046784f8035e92a41087a924ea5f504ac94f
Chris Olivier <chris.olivier@cerebras.net>
2021-03-07 04:17:20 -0800

    further simplify test script

--- examples/test_train_while.py
+++ examples/test_train_while.py
@@ -3,6 +3,7 @@ Configurable simple model runner for PyTorch
 Based upon the train_test_mnist.py script from the pytorch/xla package
 """
 
+
 def _preempt_uname():
     """
     Avoid a fork() when calling uname() in order to aid gdb
@@ -59,22 +60,22 @@ import ptwse.stats as stats
 
 import args_parse
 
-FLAGS = args_parse.parse_common_options(
-    datadir='/tmp/mnist-data',
-    batch_size=4,
-    momentum=0.5,
-    lr=0.01,
-    target_accuracy=98.0,
-    num_epochs=2)
-
+FLAGS = args_parse.parse_common_options(datadir='/tmp/mnist-data',
+                                        batch_size=4,
+                                        momentum=0.5,
+                                        lr=0.01,
+                                        target_accuracy=98.0,
+                                        num_epochs=2)
 
 FLAGS.steps_per_epoch = 50
 FLAGS.run_test = False
 FLAGS.step_print_interval = 10
-FLAGS.use_autograph = True
+FLAGS.use_autograph = False
 FLAGS.with_while = False
+FLAGS.with_if = False
 FLAGS.log_steps = 1
 FLAGS.use_fx = False
+
 
 class MNIST(nn.Module):
     def __init__(self, flags):
@@ -93,10 +94,11 @@ class MNIST(nn.Module):
                 counter = counter + 1
 
         # Try an "if" statement
-        if counter < 4:
-            counter = counter + 1
-        else:
-            counter = counter - 1
+        if FLAGS.with_if:
+            if counter < 4:
+                counter = counter + 1
+            else:
+                counter = counter - 1
         return F.log_softmax(x, dim=1)
 
 
@@ -133,45 +135,27 @@ def train_mnist(FLAGS):
 
     torch.manual_seed(1)
 
-    dims = (
-        FLAGS.batch_size,
-        1,
-        784,
-    )
+    dims = (FLAGS.batch_size, 1, 784)
 
     train_dataset_len = FLAGS.steps_per_epoch if FLAGS.steps_per_epoch else 60000
     train_loader = xu.SampleGenerator(
         data=(
-            torch.ones(dims, dtype=DTYPE,),
+            torch.ones(
+                dims,
+                dtype=DTYPE,
+            ),
             torch.ones(
                 FLAGS.batch_size,
                 dtype=torch.int64,
             ),
         ),
-        sample_count=train_dataset_len
-        // FLAGS.batch_size
-        // xm.xrt_world_size(),
-    )
-    test_loader = xu.SampleGenerator(
-        data=(
-            torch.ones(dims, dtype=DTYPE,),
-            torch.ones(
-                FLAGS.batch_size,
-                dtype=torch.int64,
-            ),
-        ),
-        sample_count=10000 // FLAGS.batch_size // xm.xrt_world_size(),
+        sample_count=train_dataset_len // FLAGS.batch_size //
+        xm.xrt_world_size(),
     )
 
-    devices = (
-        xm.get_xla_supported_devices(max_devices=FLAGS.num_cores)
-        if FLAGS.num_cores != 0
-        else []
-    )
+    devices = (xm.get_xla_supported_devices(
+        max_devices=FLAGS.num_cores) if FLAGS.num_cores != 0 else [])
 
-    """ 
-    Non multi-processing
-    """
     # Scale learning rate to num cores
     lr = FLAGS.lr * max(len(devices), 1)
 
@@ -191,8 +175,7 @@ def train_mnist(FLAGS):
             now_time = time.time()
             if hasattr(ctx, 'start_time') and ctx.start_time:
                 per_step_time = (now_time - ctx.start_time) / (
-                    ctx.step - ctx.last_step_timed
-                )
+                    ctx.step - ctx.last_step_timed)
                 steps_per_second = 1 / per_step_time
                 print(
                     f'[{xm.get_ordinal()}] Round-trip step time: '
@@ -231,6 +214,7 @@ def train_mnist(FLAGS):
         tracker = xm.RateTracker()
 
         model.train()
+        loss = None
 
         def train_inner_loop_fn(batch, ctx):
             step = ctx.step
@@ -248,13 +232,9 @@ def train_mnist(FLAGS):
                 barrier=False,
             )
 
-            if (
-                FLAGS.log_steps != 0
-                and (
-                    FLAGS.log_steps == 1
-                    or (step > 0 and step % FLAGS.log_steps == 0)
-                )
-            ):
+            if (FLAGS.log_steps != 0
+                    and (FLAGS.log_steps == 1 or
+                         (step > 0 and step % FLAGS.log_steps == 0))):
                 xm.add_step_closure(
                     _train_update,
                     args=(device, step, loss, tracker, epoch, writer),
@@ -264,9 +244,11 @@ def train_mnist(FLAGS):
                 xm.master_print(f"End TRAIN step {step}")
 
             ctx.step += 1
+            print(f"loss is on device: {loss.device}")
             return [loss]
 
         step = 0
+        outputs = None
         # Train
         print('Starting new epoch train loop... (epoch={epoch})')
         for step, (data, target) in enumerate(loader):
@@ -279,67 +261,26 @@ def train_mnist(FLAGS):
                 assert False  # Will do this shortly
             elif FLAGS.use_autograph:
                 outputs = ptwse.flow.runner.maybe_run_converted(
-                    train_inner_loop_fn,
-                    (data, target),
-                    context
-                )
+                    train_inner_loop_fn, (data, target), context)
             else:
                 outputs = train_inner_loop_fn((data, target), context)
 
         xm.master_print(f"Saving model...")
         _save_checkpoint(FLAGS, device, None, model, is_epoch=True)
         xm.master_print(f"Model saved")
-
-    def test_loop_fn(model, loader, device, context):
-        print("***********************")
-        print("ENTERING TEST FUNCTION")
-        print("***********************")
-        print('Evaluating...')
-        total_samples = 0
-        correct = 0
-        model.eval()
-        for step, (data, target) in enumerate(loader):
-            if step >= FLAGS.test_max_step:
-                break
-            output = model(data)
-            pred = output.max(1, keepdim=True)[1]
-            if FLAGS.mp:
-                correct += pred.eq(target.view_as(pred)).sum()
-            else:
-                correct += pred.eq(target.view_as(pred)).sum().item()
-            total_samples += data.size()[0]
-
-        if FLAGS.mp:
-            this_accuracy = 100.0 * correct.item() / total_samples
-            print("CALLING: mesh_reduce('test_accuracy')")
-            this_accuracy = xm.mesh_reduce(
-                'test_accuracy', this_accuracy, np.mean
-            )
-            print("BACK FROM: mesh_reduce('test_accuracy')")
-        else:
-            this_accuracy = 100.0 * correct / total_samples
-            test_utils.print_test_update(device, this_accuracy)
-        print("***********************")
-        print("LEAVING TEST FUNCTION")
-        print("***********************")
-        return this_accuracy
+        return loss
 
     #
-    # Set up for
+    # Set up
     #
-    accuracy = 0.0
-
-    num_devices = (
-        len(xm.xla_replication_devices(devices)) if len(devices) > 1 else 1
-    )
+    num_devices = (len(xm.xla_replication_devices(devices))
+                   if len(devices) > 1 else 1)
 
     if not FLAGS.steps_per_epoch:
-        num_training_steps_per_epoch = train_dataset_len // (
-            FLAGS.batch_size * num_devices
-        )
+        num_training_steps_per_epoch = train_dataset_len // (FLAGS.batch_size *
+                                                             num_devices)
     else:
         num_training_steps_per_epoch = FLAGS.steps_per_epoch
-    max_accuracy = 0.0
 
     #
     # Epoch loop
@@ -352,58 +293,21 @@ def train_mnist(FLAGS):
         ctx = dp.Context(device=device)
         ctx.tracker = xm.RateTracker()
         ctx.step = 0
-        train_loop_fn(model, train_loader, device, ctx)
-
-        #
-        # Test
-        #
-        if FLAGS.run_test:
-            with ptwse.scope.proxy_disabled(disabled=FLAGS.test_off_proxy):
-                accuracies = model_parallel(test_loop_fn, test_loader)
-            accuracy = mean(accuracies)
-            print(
-                'Epoch: {}, Mean Accuracy: {:.2f}%'.format(epoch, accuracy)
-            )
-
-        global_step = (epoch - 1) * num_training_steps_per_epoch
-        max_accuracy = max(accuracy, max_accuracy)
-
-        test_utils.write_to_summary(
-            writer,
-            global_step,
-            dict_to_write={'Accuracy/test': accuracy},
-            write_xla_metrics=True,
-        )
+        loss = model_parallel(train_loop_fn, train_loader)
 
         if FLAGS.metrics_debug:
             xm.master_print(met.metrics_report())
 
     test_utils.close_summary_writer(writer)
-    xm.master_print('Max Accuracy: {:.2f}%'.format(max_accuracy))
-    return max_accuracy
-
-
-class TrainMnist(unittest.TestCase):
-    def __init__(self, flags):
-        self._flags = flags
-
-    def tearDown(self):
-        super(TrainMnist, self).tearDown()
-        if self._flags.tidy and os.path.isdir(self._flags.datadir):
-            shutil.rmtree(self._flags.datadir)
-
-    def test_accurracy(self):
-        if not self._flags.fake_data:
-            self.assertGreaterEqual(train_mnist(), self._flags.target_accuracy)
-        else:
-            train_mnist()
+    return loss
 
 
 def main(args):
     import argparse
     import traceback
-    
-    os.environ['XRT_DEVICE_MAP'] = 'CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0'
+
+    os.environ[
+        'XRT_DEVICE_MAP'] = 'CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0'
     os.environ['XRT_WORKERS'] = 'localservice:0;grpc://localhost:40934'
 
     try:
